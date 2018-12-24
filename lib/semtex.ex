f3df2314f03b7aeae9b72b9ad785036c37fd4d60
Alizain Feerasta <alizain.feerasta@gmail.com>
2018-12-24 17:51:57 +0500

    Generate mix project

---
+++ lib/semtex.ex
@@ -0,0 +1,18 @@
+defmodule Semtex do
+  @moduledoc """
+  Documentation for Semtex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Semtex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/semtex_test.exs
@@ -0,0 +1,8 @@
+defmodule SemtexTest do
+  use ExUnit.Case
+  doctest Semtex
+
+  test "greets the world" do
+    assert Semtex.hello() == :world
+  end
+end
